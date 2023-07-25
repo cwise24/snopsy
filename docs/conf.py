40891ee2ef7cb50547d4ddb29543d2d61dc33491
Chad <cwise24@users.noreply.github.com>
2023-07-25 08:45:53 -0700

    Update 2023

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'SNOPsy'
-copyright = '2022, Chad Wise'
+copyright = '2023, Chad Wise'
 author = 'Chad Wise'
 
 # The full version, including alpha/beta/rc tags
